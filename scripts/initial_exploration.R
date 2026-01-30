e0ba882135cfd79c10a47e10df6df3d302b12844
violapombeiro <uxj25bvu@uea.ac.uk>
2026-01-30 13:59:58 +0000

    added R script to view mosquito data.

---
+++ scripts/initial_exploration.R
@@ -0,0 +1,42 @@
+# Week 1: Initial Data Exploration ====
+# Author: Viola
+# Date: 20/01/2026
+
+# Load packages ====
+library(tidyverse)
+library(here)
+library(naniar)
+library(janitor)
+library(skimr)
+# Load data ====
+mosquito_egg_raw <- read_csv(here("data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this?
+#   A mosquito. 
+# - What's being measured?
+#   The amount of eggs laid and hatched, when under different doses of a drug
+# - How many observations?
+#   
+# - Anything surprising?
+#   
+# - Any obvious problems?
+#
