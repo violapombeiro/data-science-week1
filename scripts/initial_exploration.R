215e2b298a6da87e20a272923c39c3665e12d8b2
violapombeiro <uxj25bvu@uea.ac.uk>
2026-02-06 14:26:10 +0000

    Adding cleaning steps to raw data

--- scripts/initial_exploration.R
+++ scripts/initial_exploration.R
@@ -43,3 +43,72 @@ mosquito_egg_raw |>
 # -   Sites have issues with syntax - Site-A/Site A, HIGH_DOSE / Dose
 # -   body_mass_mg has a lowest value of -93.0 - this is impossible
 # -   Some values are N/A - check entry fields
+
+# FIX 1: Incorrect string spelling ====
+
+# Show the problem:
+# [Code to demonstrate issue exists]
+mosquito_egg_raw |> distinct(site)
+mosquito_egg_raw |> distinct(treatment)
+# Fix it:
+mosquito_egg_data_step1 <- mosquito_egg_raw |>
+  mutate(site = case_when(
+    site == "Site_C" ~ "Site C",
+    site == "Site-C" ~ "Site C",
+    site == "site_c" ~ "Site C",
+    site == "Site_A" ~ "Site A",
+    site == "site_a" ~ "Site A",
+    site == "Site-A" ~ "Site A",
+    site == "Site_B" ~ "Site B",
+    site == "site_b" ~ "Site B",
+    site == "Site-B" ~ "Site B",
+    .default = as.character(site)
+   )
+           )
+mosquito_egg_data_step1 <- mosquito_egg_data_step1 |>
+  mutate(treatment = case_when(
+    treatment == "CONTROL" ~ "Control",
+    treatment == "control" ~ "Control",
+    treatment == "Low_dose" ~ "Low Dose",
+    treatment == "low_dose" ~ "Low Dose",
+    treatment == "LOW_DOSE" ~ "Low Dose",
+    treatment == "Medium_dose" ~ "Medium Dose",
+    treatment == "MEDIUM_DOSE" ~ "Medium Dose",
+    treatment == "medium_dose" ~ "Medium Dose",
+    treatment == "High_dose" ~ "High Dose",
+    treatment == "high_dose" ~ "High Dose",
+    treatment == "HIGH_DOSE" ~ "High Dose",
+    .default = as.character(treatment)
+  ))
+mosquito_egg_data_step1 <- mosquito_egg_data_step1 |>
+  mutate(collector = case_when(
+    collector == "Garci" ~ "Garcia",
+    collector == "Smyth" ~ "Smith",
+    .default = as.character(collector)
+  ))
+  # Verify it worked:
+  mosquito_egg_data_step1 |> distinct(site)
+  mosquito_egg_data_step1 |> distinct(treatment)
+  mosquito_egg_data_step1 |> distinct(collector)
+  # What changed and why it matters:
+  # distinct() allows visualization of each unique string for site/treatment
+  # Mutate() then allows us to change each string to a string of our choice,
+  # standardising the answers
+  
+  
+  # FIX 2: [Issue description]  ====
+
+# Show the problem:
+# [Code]
+
+# Fix it:
+mosquito_egg_data_step2 <- mosquito_egg_data_step1 |>
+  # YOUR CODE
+  
+  
+  # Verify it worked:
+  # [Code]
+  
+  # What changed and why it matters:
+  # [2-3 sentences]
+  #
